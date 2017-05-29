b59782d2b0a146c5a0d8f762b02839e3fc13f713
baturay alhaj ahmad <thebatu@gmail.com>
2017-05-29 11:00:23 +0200

    clean code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,10 +22,10 @@ $(function(){
   function success(pos) {
     var crd = pos.coords;
 
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
+    // console.log('Your current position is:');
+    // console.log(`Latitude : ${crd.latitude}`);
+    // console.log(`Longitude: ${crd.longitude}`);
+    // console.log(`More or less ${crd.accuracy} meters.`);
 
     $('#check_latitude').val(crd.latitude);
     $('#check_longitude').val(crd.longitude);
