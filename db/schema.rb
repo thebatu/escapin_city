27483bbf0489d075cc7279df1fc56064ddd63807
Heritiana Rabary <marvduck7@gmail.com>
2017-05-30 18:36:24 +0200

    index images seeded and redone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525084559) do
+ActiveRecord::Schema.define(version: 20170530140417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170525084559) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "photo"
   end
 
   create_table "checkpoints", force: :cascade do |t|
