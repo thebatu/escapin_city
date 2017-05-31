dd9748a88636bbf5f386f0fd42861e4104ca5a67
baturay alhaj ahmad <thebatu@gmail.com>
2017-05-31 18:58:59 +0200

    change distance check

--- app/controllers/hunts_controller.rb
+++ app/controllers/hunts_controller.rb
@@ -61,7 +61,7 @@ class HuntsController < ApplicationController
 
     distance = Geocoder::Calculations.distance_between(loc_nav,loc_checkpoint)
 
-    if distance < (70 + accuracy.to_f) # 70 supposed min distance to treasure
+    if distance > (1 + accuracy.to_f) # 70 supposed min distance to treasure
       @checkpoint = @current_checkpoint.lower_item
       @show_content = @checkpoint.content
       @checkpoint_fail = false
